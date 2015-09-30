dd9cb71cfc207ff7ff238f52417a4457e176d901
Jeff George <webdevjeffus@gmail.com>
2015-09-30 00:39:25 -0400

    Add 4.3.2 math-methods

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ num_1.to_f / num_2.to_f
+end
