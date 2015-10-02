c3642f7ed09f1e34f93599fffbcc8afe8c634704
Jeff George <webdevjeffus@gmail.com>
2015-10-01 21:06:41 -0400

    Add letter_grade challenge

---
+++ week-4/pair4-5/get-grade-ps.md
@@ -0,0 +1,14 @@
+# Pseudocode for Get Grade Challenge
+
+PRINT Enter a grade?
+
+INPUT a numeric grade
+
+IF grade below 60, grade is F
+ELSE grade below 70, grade is D
+ELSE grade below 80, grade is C
+ELSE grade below 90, grade is B
+ELSE grade is A
+
+RETURN grade
+
---
+++ week-4/pair4-5/get_grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge Jeff/Max.
+
+
+# Your Solution Below
+
+def get_grade(number)
+
+  if number < 60 then return "F"
+
+  elsif number < 70 then return "D"
+
+  elsif number < 80 then return "C"
+
+  elsif number < 90 then return "B"
+
+  else return "A"
+
+  end
+
+end
