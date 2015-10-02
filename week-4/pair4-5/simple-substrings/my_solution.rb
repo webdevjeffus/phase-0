deb82db5a458e3ab4b3311d197593ab55365f777
Jeff George <webdevjeffus@gmail.com>
2015-10-01 23:11:15 -0400

    Add simple_substrings challenge

---
+++ week-4/pair4-5/simple-substrings/my_solution.rb
@@ -0,0 +1,25 @@
+# Simple Substrings
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def welcome(address)
+
+  if address.include?("CA")
+
+    return "Welcome to California"
+
+  else
+
+    return "You should move to California"
+
+  end
+end
+
+# puts "Place in CA"
+# puts welcome("Place in CA")
+#
+# puts "Place in NY"
+# puts welcome("Place in NY")
