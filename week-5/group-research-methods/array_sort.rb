5152edb9543800317bddbdf289e49676afd3e937
Jeff George <webdevjeffus@gmail.com>
2015-10-09 02:08:15 -0400

    Group Research: Add solutions for #3, #4, #5

--- week-5/group-research-methods/array_sort.rb
+++ week-5/group-research-methods/array_sort.rb
@@ -3,21 +3,30 @@
 
 
 def my_array_sorting_method(source)
-  p source
-  sorted = source.each{ |item| item.to_s }
-  p source
-  p sorted
+  source = source.partition{ |item| item.is_a? Integer }
+  source[0] = source[0].sort
+  source[1] = source[1].sort
+  source.flatten
+end
 
 
+def my_array_sorting_method(source)
+  source.sort_by{ |x| x.to_s.downcase }
+end
+
+def my_array_sorting_method!(source)
+  source.sort_by!{ |x| x.to_s }
 end
 
 
 
-
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
+# i_want_pets = ["I", "want", "pets", "but", "only", "have"]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
-my_array_sorting_method(i_want_pets)
-
+p i_want_pets.object_id
+p new_array = my_array_sorting_method(i_want_pets)
+p new_array.object_id
+p i_want_pets.object_id
 # $ my_array_sorting_method(i_want_pets)
 # => [2, 3, "I", "but", "have", "only", "pets", "want"]
--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -34,49 +34,142 @@ end
 #
 
 
-# Person 3
+# Person 3 - Jeff George
+# Array sort initial solution:
 def my_array_sorting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source = source.partition{ |item| item.is_a? Fixnum }
+  source[0] = source[0].sort
+  source[1] = source[1].sort
+  source.flatten
 end
 
+# Array sort refactored solution:
+def my_array_sorting_method(source)
+  source.sort_by{ |x| x.to_s }
+end
+
+# Destructive version of array sort
+# (Not the correct solution; shown for comparison to correct, non-destructive version):
+def my_array_sorting_method!(source)
+  source.sort_by!{ |x| x.to_s }
+end
+
+# Hash sort solution
 def my_hash_sorting_method(source)
-   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+   source.sort_by{ |k,v| v}
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# Both of my final solutions use the **Enumerable#sort_by** method, which
+# sorts the contents of the collection according to the given block of code.
+# For both hashes and arrays, #sort_by is non-destructive. There is a
+# destructive version for Arrays only, **Array#sort_by!**, which operates on
+# the original array instead of returning a new array. I have included a
+# destructive version of the method, #my_array_sorting_method!, for comparison.
+# (Because it is never called by the rpsec file, the presence of
+# #my_array_sorting_method! in this file does not affect the outcome of the
+# tests.)
+
+# Array version
+# The block passed to **#sort_by** causes the items in the array to be sorted
+# by their values as strings; this is accomplished by adding the **#to_s**, to
+# convert the numbers in the array into strings. While this method passes this
+# particular rspec, IT WILL FAIL if there are any numbers in the array with
+# more than a single digit in them. To make the method work for integers of any
+# size, you need to use the initial, less-streamlined solution, which uses
+# **Enumerable#partition** to sift the integers and strings into separate
+# arrays, then runs separate **Array#sort** calls on those arrays to put each
+# in order, and finally calls **Array#flatten** to turn them back into a
+# single array.
+
+# Note also that the test doesn't mind that the sort is case-sensitive; since
+# all capitals are alphabetized ahead of lower-case letters by default, the
+# first string in the sorted array is "I". For a case-insensitive sort, you
+# could add the method **String#downcase** to the end of the block, like this:
+# source.sort_by{ |x| x.to_s.downcase }, which would return this array:
+# [2, 3, "but", "have", "I", "only", "pets", "want"]
+
+# Hash version
+# Amazingly, the hash solution is actually simpler than the array solution.
+# You use the method **Enumerable#sort_by**, and simply tell it to sort by the
+# value. Doing so returns a two-dimensional array with the items sorted by the
+# value of the second element. If you actually needed the method as a sorted
+# hash, you just add the method **Array#to_h**, which converts a two-dimensional
+# array right back into a hash.
 
 
-# Person 4
+
+
+# Person 4 - Eran Chazan
+#PseudoCode for an Array
+# Accepts an array
+# iterates through the array
+# for each element, seatch for letter
+# if element contains letter, delete element
+
 def my_array_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+ source.delete_if{|word| word.to_s.include?(thing_to_delete)}
 end
-
+# PsueudoCode for a Hash
+# Accept hash and keyword
+# iterate through hash
+# if key matches key in the 'if' statement then delete key and value
+#
 def my_hash_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+source.delete_if{|pet, age| pet==thing_to_delete}
 end
 
 # Identify and describe the Ruby method(s) you implemented.
+#.delete_if => deletes every element of self which block evaluates to true.
+#.to_s => this converts an object to a string
+#.include? => this checks each object and returns true if the parameters given
+# is present in the object being checked
 #
+# Teach your accountability group how to use the methods
+# You want to use the delete_if methods when you are checking for
+# selective entries in an if statement. delete_if is like an if statement
+# in that you need to declare a function block within {} where you give it
+# an iterator and an conditional statement.
 #
-#
+#Share any tricks you used to find and decipher the Ruby Docs
+# When looking through rubydocs I looked up that most resemble my pseudocode statement.
+# If non exist then I look for candidates that could be a susbstitute. If I still can't find
+# any, then I google what I want and see if what other people use in their solutions which I
+# then look up in the RubyDocs.
 
 
-# Person 5
+
+
+
+
+# Person 5 - Steven Broderick
 def my_array_splitting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.partition { |item| item.is_a? Integer }
 end
 
 def my_hash_splitting_method(source, age)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.partition { |key, val| val <= age }
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+
+# my_array_splitting_method takes an array as its argument and splits the array into two arrays, one with
+#   integers (ages, here) and one with everything else (names, in this case). Then it returns an array containing
+#   both of those arrays. The result is a 2-dimensional array. The method takes advantage of the #partition and
+#   #is_a? methods:
+
+# #partition splits the items into two arrays, putting those that match the condition into one array
+#   and those don't into the other.
+
+# #is_a? takes a class name as its argument and returns true or false, depending on whether
+#   the object calling the method is a member of the argument class.
+
+# my_hash_splitting_method takes two arguments: (1) a hash that has integers for its values (ages, here) and (2)
+#   an integer (also an age, in this case). It splits the hash into two arrays, one containing the key-value pairs
+#   (as 2-element arrays) for which the value (age) is less than or equal to the "age" argument and one for everything
+#   else. Then it returns an array containing both of those arrays. The result is a 3-dimensional array. This method
+#   also takes advantage of #partition, but the condition is different from the one in the other method.
+
 
 
 # Release 1: Identify and describe the Ruby method you implemented. Teach your
