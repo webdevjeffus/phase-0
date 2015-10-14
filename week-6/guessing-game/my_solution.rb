a665c59757fb50335ed957f955f828a15f882f83
Jeff George <webdevjeffus@gmail.com>
2015-10-13 23:48:31 -0400

    Create initial files for 6.3 Guessing Game

---
+++ week-6/guessing-game/my_solution.rb
@@ -0,0 +1,34 @@
+# Build a simple guessing game
+
+
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# Initial Solution
+
+class GuessingGame
+  def initialize(answer)
+    # Your initialization code goes here
+  end
+
+  # Make sure you define the other required methods, too
+end
+
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# Reflection
