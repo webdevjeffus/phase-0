58f9c15da7e86fd1e67240b65a503bef7ffb789a
Jeff George <webdevjeffus@gmail.com>
2015-10-13 22:46:58 -0400

    Add files for die-class challenge 6.2

---
+++ week-6/die-class/my_solution.rb
@@ -0,0 +1,38 @@
+# Die Class 2: Arbitrary Symbols
+
+
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# Initial Solution
+
+class Die
+  def initialize(labels)
+  end
+
+  def sides
+  end
+
+  def roll
+  end
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+
+# Reflection
