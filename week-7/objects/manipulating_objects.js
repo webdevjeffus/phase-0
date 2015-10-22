0eeafc0af531a02e70bb16a6df4f302c1a94b771
Jeff George <webdevjeffus@gmail.com>
2015-10-21 22:11:24 -0400

    Add reflection to 7.4 manipulating_objects.js

--- week-7/objects/manipulating_objects.js
+++ week-7/objects/manipulating_objects.js
@@ -1,6 +1,6 @@
  // Manipulating JavaScript Objects
 
-// I worked on this challenge: [by myself, with: ]
+// I worked on this challenge by myself.
 
 // There is a section below where you will write your code.
 
@@ -53,17 +53,42 @@ adam.children = terah.children;
 
 
 
-// __________________________________________
-// Reflection: Use the reflection guidelines
-//
-//
-//
-//
-//
-//
+/* ========================================================================= *\
+
+REFLECTION
+
+What tests did you have trouble passing? What did you do to make it pass?
+Why did that work?
+
+I had a little trouble figuring out the proper syntax for adding the first
+child, carson, to the children object inside the terah object. After trying
+a couple of different things which didn't work, I just googled for "add
+property to javascript object", which led to a W3Schools page which reminded
+me of what I already knew--that I the syntax was object.newKey = value. I
+stopped at that point think about how to name the children object within the
+terah object, and realized that I simply had to chain key names with dots to i
+dentify the key within the nested object. Then I splapped myself on the
+forehead, and typed in the proper line of code.
 
 
-// __________________________________________
+How difficult was it to add and delete properties outside of the object
+itself?
+
+Adding properties using obj.propName = value was trivial. Actually deleting
+a property, as opposed to setting its value to null or 0, I couldn't guess
+on my own; I googled and wound up at Mozilla Developer's Network, where I
+read how to use the delete property.
+
+
+What did you learn about manipulating objects in this challenge?
+
+My two main takeaways from this challenge were the ones mentioned above--how
+to add a property to an object nested inside another object (or array, for
+that matter), and how to delete a property entirely from an object.
+
+
+\* ========================================================================= */
+
 // Driver Code:  Do not alter code below this line.
 function assert(test, message, test_number) {
   if (!test) {
