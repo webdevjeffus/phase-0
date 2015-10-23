1bc8941412903d7a4ccaa47a3f0248969d35a401
Jeff George <webdevjeffus@gmail.com>
2015-10-23 15:17:15 -0400

    7.7 Game: Implement Play Again pop-up at end of game

--- week-7/game/game.js
+++ week-7/game/game.js
@@ -124,8 +124,8 @@ else {
 }
 */
 
-/*************************** INITIAL APP VERSION *****************************\
-\*     Open game.html to play; plays through interactive app in browser.     */
+/*************************** PLAYABLE APP VERSION ****************************\
+\*     Open game.html to play; plays through interactive app in browser.     *\
 
 "use strict";
 
@@ -157,7 +157,6 @@ var playByPlay = document.getElementById("playByPlay");
 var turnScore = document.getElementById("turnScore");
 var playerPt = document.getElementsByClassName("playerPt");
 var oppoPt = document.getElementsByClassName("oppoPt");
-
 
 function getPlayerMove(move) {
   player.move = move;
@@ -271,6 +270,196 @@ function checkForWin() {
 }
 
 
+/************************ FULL-FEATURED APP VERSION **************************\
+\*     Open game.html to play; plays through interactive app in browser.     */
+
+"use strict";
+
+// GAME OBJECTS
+var game = {
+  drawRound: " This round is a draw.",
+  winRound: " You win this round.",
+  loseRound: " You lose this round.",
+  winGameHead: "Congratulations!",
+  loseGameHead: "You've lost this game.",
+  winGameMsg: "You've won the game!",
+  loseGameMsg: "Better luck next time!",
+  rockScissors: "Rock breaks scissors.",
+  scissorsPaper: "Scissors cut paper.",
+  paperRock: "Paper covers rock."
+};
+
+var player = {
+  move: "?",
+  score: 0
+};
+
+var oppo = {
+  moves: ["rock", "paper", "scissors"],
+  move: "?",
+  score: 0
+};
+
+// DOM HANDLES
+var playerMove = document.getElementById("playerMove");
+var oppoMove = document.getElementById("oppoMove");
+var playByPlay = document.getElementById("playByPlay");
+var turnScore = document.getElementById("turnScore");
+var playerPt = document.getElementsByClassName("playerPt");
+var oppoPt = document.getElementsByClassName("oppoPt");
+var gameOverBox = document.getElementById("gameOverBox");
+var gameOverHead = document.getElementById("gameOverHead");
+var gameOverMsg = document.getElementById("gameOverMsg");
+
+// GAME FUNCTIONS
+function getPlayerMove(move) {
+  player.move = move;
+  getOppoMove();
+  displayMoves();
+  resolveTurn();
+  checkForWin();
+}
+
+function getOppoMove() {
+  oppo.move = oppo.moves[ Math.floor(Math.random() * 3) ];
+}
+
+function displayMoves() {
+  switch (player.move) {
+    case "rock":
+      playerMove.innerHTML = "<i class='fa fa-hand-rock-o'></i>";
+      break;
+    case "paper":
+      playerMove.innerHTML = "<i class='fa fa-hand-paper-o'></i>";
+      break;
+    default: // "scissors"
+      playerMove.innerHTML = "<i class='fa fa-hand-scissors-o'></i>";
+      break;
+    }
+
+    switch (oppo.move) {
+    case "rock":
+      oppoMove.innerHTML = "<i class='fa fa-hand-rock-o'></i>";
+      break;
+    case "paper":
+      oppoMove.innerHTML = "<i class='fa fa-hand-paper-o'></i>";
+      break;
+    default: // "scissors"
+      oppoMove.innerHTML = "<i class='fa fa-hand-scissors-o'></i>";
+      break;
+    }
+}
+
+
+function resolveTurn() {
+  // IF both players chose same move
+  if ( player.move === oppo.move ) {
+    playByPlay.innerHTML = ( "Both players chose " + player.move + ".");
+    turnScore.innerHTML = ( game.drawRound );
+  }
+
+  // ELSE players chose different moves
+  else {
+    if ( player.move === "rock" ) {
+      if ( oppo.move === "paper" ) {
+        playByPlay.innerHTML = ( game.paperRock );
+        turnScore.innerHTML = ( game.loseRound );
+        oppoPt[oppo.score].setAttribute(
+          "style",
+          "background-color: red; border: 2px darkred solid;"
+        );
+        oppo.score++;
+      }
+      else { // oppo.move === "scissors"
+        playByPlay.innerHTML = ( game.rockScissors );
+        turnScore.innerHTML = ( game.winRound );
+        playerPt[player.score].setAttribute(
+          "style",
+          "background-color: green; border: 2px darkgreen solid;"
+        );
+        player.score++;
+      }
+    }
+    else if ( player.move === "paper" ) {
+      if ( oppo.move === "scissors" ) {
+        playByPlay.innerHTML = ( game.scissorsPaper );
+        turnScore.innerHTML = ( game.loseRound );
+        oppoPt[oppo.score].setAttribute(
+          "style",
+          "background-color: red; border: 2px darkred solid;"
+        );
+        oppo.score++;
+      }
+      else { // oppo.move === "rock"
+        playByPlay.innerHTML = ( game.paperRock);
+        turnScore.innerHTML = ( game.winRound );
+        playerPt[player.score].setAttribute(
+          "style",
+          "background-color: green; border: 2px darkgreen solid;"
+        );
+        player.score++;
+      }
+    }
+    else { // player.move === "scissors"
+      if ( oppo.move === "rock" ) {
+        playByPlay.innerHTML = ( game.rockScissors );
+        turnScore.innerHTML = ( game.loseRound );
+        oppoPt[oppo.score].setAttribute(
+          "style",
+          "background-color: red; border: 2px darkred solid;"
+        );
+        oppo.score++;
+      }
+      else { // oppo.move === "paper"
+        playByPlay.innerHTML = ( game.scissorsPaper );
+        turnScore.innerHTML = ( game.winRound );
+        playerPt[player.score].setAttribute(
+          "style",
+          "background-color: green; border: 2px darkgreen solid;"
+        );
+        player.score++;
+      }
+    }
+  }
+  console.log(player.score + ", " + oppo.score);
+}
+
+function checkForWin() {
+  console.log("checkForWin fired.");
+  if (player.score === 3) {
+    console.log("Player wins.");
+    gameOverBox.setAttribute( "style", "display: block;" );
+      //"style",
+      //"display: block; background-color: lightgreen; border: 2px darkgreen solid;"
+      //"display: block;"
+    //);
+    gameOverHead.innerHTML = game.winGameHead;
+    gameOverMsg.innerHTML = game.winGameMsg;
+  }
+  else if (oppo.score === 3) {
+    console.log("Opponent wins.");
+    gameOverBox.setAttribute( "style", "display: block;" );
+      //"style",
+      // "display: block; background-color: rgb(256,164,164); border: 2px rgb(128,0,0) solid;"
+      //"display: block;"
+    //);
+    gameOverHead.innerHTML = game.loseGameHead;
+    gameOverMsg.innerHTML = game.loseGameMsg;
+  }
+  else { }
+}
+
+function playAgain() {
+  gameOverBox.setAttribute("style", "display: hidden;");
+  for (var i = 0; i < 3; i++) {
+    playerPt[i].setAttribute("style", "background-color: #ddd; border-color: #888;");
+    oppoPt[i].setAttribute("style", "background-color: #ddd; border-color: #888;");
+  }
+  playerMove.innerHTML = ("<i class='fa fa-question'></i>");
+  oppoMove.innerHTML = ("<i class='fa fa-question'></i>");
+  player.score = 0;
+  oppo.score = 0;
+}
 
 // Reflection
 //
