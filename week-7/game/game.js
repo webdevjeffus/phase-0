e4abdf6b5e904e6f48ba0728e896f1e38b55ebc2
Jeff George <webdevjeffus@gmail.com>
2015-10-22 21:42:43 -0400

    MVP for prompt-based Rock-Paper_Scissors game, with base html/css page.

--- week-7/game/game.js
+++ week-7/game/game.js
@@ -1,26 +1,123 @@
- // Design Basic Game Solo Challenge
+/* Design Basic Game Solo Challenge
 
-// This is a solo challenge
+This is a solo challenge
 
-// Your mission description:
-// Overall mission:
-// Goals:
-// Characters:
-// Objects:
-// Functions:
+Your mission description:
+Overall mission: Play Rock-Paper-Scissors
+Goals: Win best 3-out-of-5 rounds of RPS
+Characters: Player, Opponent
+Objects: Game, Player, Opponent
+Functions:
 
-// Pseudocode
-//
-//
-//
-//
-//
+Pseudocode:
+Objects:
+  PLAYER
+    player's move
+    player's score
+
+  OPPONENT
+    opponent's move
+    opponent's score
+
+Functions:
+  Get player's move
+  Choose opponent's move
+  Resolve turn
+  Check for winner
+
+*/
+
 
 // Initial Code
+var game = {
+  drawRound: " This round is a draw.",
+  winRound: " You win this round.",
+  loseRound: " You lose this round.",
+  winGame: "Congratulations! You've won the game!",
+  loseGame: "You've lost this game. Better luck next time!",
+  rockScissors: "Rock breaks scissors.",
+  scissorsPaper: "Scissors cut paper.",
+  paperRock: "Paper covers rock.",
+};
 
+var player = {
+  move: null,
+  score: 0,
+};
 
+var oppo = {
+  moves: ["R", "P", "S"],
+  move: null,
+  score: 0,
+};
 
+function getPlayerMove() {
+  player.move = prompt("Enter (R)ock, (P)aper, or (S)cissors:");
+  player.move = player.move.toUpperCase();
+  alert("You picked " + player.move);
+};
 
+function getOppoMove() {
+  oppo.move = oppo.moves[Math.floor(Math.random() * 3)];
+  alert("Your opponent picked " + oppo.move);
+}
+
+function resolveTurn() {
+  if ( player.move === oppo.move ) {
+    alert( "Both players chose " + player.move + "."+ game.drawRound );
+  }
+
+  else {
+    if ( player.move === "R" ) {
+      if ( oppo.move === "P" ) {
+        alert( game.paperRock + game.loseRound );
+        oppo.score++;
+      }
+      else { // oppo.move === "S"
+        alert( game.rockScissors + game.winRound );
+        player.score++;
+      }
+    }
+    else if ( player.move === "P" ) {
+      if ( oppo.move === "S" ) {
+        alert( game.scissorsPaper + game.loseRound );
+        oppo.score++;
+      }
+      else { // oppo.move === "R"
+        alert( game.paperRock + game.winRound );
+        player.score++;
+      }
+    }
+    else { // player.move === "S"
+      if ( oppo.move === "R" ) {
+        alert( game.rockScissors + game.loseRound );
+        oppo.score++;
+      }
+      else {// oppo.move === "P"
+        alert( game.scissorsRock + game.winRound );
+        player.score++;
+      }
+    }
+  }
+}
+
+function reportScore() {
+  alert("You have " + player.score + " points. Your opponent has " + oppo.score + " points.");
+}
+
+while (player.score < 3 && oppo.score < 3) {
+  getPlayerMove();
+  getOppoMove();
+  resolveTurn();
+  reportScore();
+}
+
+if (player.score === 3) {
+  alert(game.winGame);
+}
+else {
+  alert(game.loseGame);
+}
 
 
 // Refactored Code
