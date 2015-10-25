7ecc35da5d193a4c575f7dcd65c3b6c696febf12
Jeff George <webdevjeffus@gmail.com>
2015-10-25 13:57:20 -0400

    7.6 Olympics: Create initial files

---
+++ week-7/olympics/javascript_olympics.js
@@ -0,0 +1,33 @@
+// JavaScript Olympics
+
+// I paired with Marie-France Han on this challenge.
+
+// This challenge took me [#] hours.
+
+
+// Warm Up
+
+
+
+
+// Bulk Up
+
+
+
+
+// Jumble your words
+
+
+
+
+// 2,4,6,8
+
+
+
+
+// "We built this city"
+
+
+
+
+// Reflection
