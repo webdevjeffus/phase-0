bc6af142e3b74e2589b0ff24bd8d817c6e001966
Jeff George <webdevjeffus@gmail.com>
2015-11-07 17:30:04 -0500

    9.5 Car Class: Add final solution with reflection.

--- week-9/car-class/review.rb
+++ week-9/car-class/review.rb
@@ -64,6 +64,7 @@ Create pizza class with arguments for order number, size, toppings
 
 # 3. Initial Solution
 
+=begin
 class Pizza
   attr_reader :size, :toppings, :order_num
   @@current_pizza_id = 1
@@ -127,13 +128,72 @@ class Car
   end
 
 end
-
-
-
+=end
 
 # 4. Refactored Solution
 
+class Pizza
+  attr_reader :size, :toppings, :pizza_id
+  @@current_pizza_id = 1
 
+  def initialize(size, toppings=[])
+    @size = size
+    @toppings = toppings.to_a
+    @pizza_id = @@current_pizza_id
+    @@current_pizza_id += 1
+  end
+end
+
+
+class Car
+  attr_reader :model, :color, :compass, :direction, :odometer, :hotbox
+  attr_accessor :distance, :speed
+
+  def initialize(model, color)
+    @model = model.capitalize
+    @color = color
+    @compass = ["North", "East", "South", "West"]
+    @direction = 0
+    @odometer = 0
+    @hotbox = []
+    puts "I'm delivering pizzas in a #{@color} #{@model}!"
+  end
+
+  def turn(right_or_left)
+    if right_or_left == "right" then @direction += 1
+    elsif right_or_left == "left" then @direction -= 1
+    end
+
+    if @direction < 0 then @direction = 3
+    elsif @direction > 3 then @direction = 0
+    end
+  end
+
+  def route_leg( speed, distance, leg_turn="straight")
+    self.turn(leg_turn)
+    @speed = speed
+    @distance = distance
+    @odometer += distance
+    if leg_turn != "straight"
+      puts "I'm turning #{leg_turn}, and driving #{distance} miles to the #{self.compass[self.direction]}."
+    else
+      puts "I'm driving #{distance} more miles to the #{self.compass[self.direction]}."
+    end
+  end
+
+  def load_pizza(size, toppings=[])
+    @hotbox << Pizza.new(size, toppings)
+    puts "Loading pizza number #{@hotbox[-1].pizza_id} #{size} #{toppings} pizza in my hotbox!"
+    puts "I now have #{@hotbox.length} pizzas to deliver!"
+  end
+
+  def deliver_pizza
+    pizza = @hotbox.shift
+    puts "Delivering pizza number #{pizza.pizza_id} #{pizza.size} #{pizza.toppings} pizza."
+    puts "There are #{@hotbox.length} pizzas left to deliver!"
+  end
+
+end
 
 
 
@@ -149,7 +209,6 @@ mustang.load_pizza("large", ["pepperoni", "anchovies"])
 mustang.load_pizza("small", ["sausage"])
 mustang.load_pizza("large", ["green pepper", "olives"])
 mustang.load_pizza("medium", ["ham", "pineapple", "green peppers"])
-
 puts "I'm delivering #{mustang.hotbox.length} pizzas on this run!"
 
 mustang.deliver_pizza
@@ -183,3 +242,41 @@ puts "I drove #{mustang.odometer} to deliver these pizzas!"
 
 
 # 5. Reflection
+=begin
+
+What concepts did you review in this challenge?
+
+Obviously, this challenge was a review of Class and object creation in Ruby--
+which was a good thing to do this week, to put JavaScript to bed and get back
+into my Ruby headspace before Phase 1. This challenge gave us the chance to
+create two classes, one of which, the Car class, is sort of a "wrapper" class
+in the program, and another, the Pizza class, that can be instantiated from
+within the wrapper class (see our Car#load_pizza method), and then those Pizza
+objects can be passed out of their owning Car object, and dealt with separately
+(see our Car#deliver_pizza method).
+
+
+What is still confusing to you about Ruby?
+
+It's not so much that I'm _confused_ about this, but I'm excited to see where
+Ruby is going. I've done some programming in Python in which it was possible to
+create graphic video games using a 2D graphic library, called Pygame. I'm
+very interested to learn how Ruby gems can be incorporated into programs, and
+what capabilities they are capable of adding. I am expecting that they will be
+the rough equivalent of Python's libraries, but that's supposition on my part.
+I'm very interested to see where Ruby goes next.
+
+
+What are you going to study to get more prepared for Phase 1?
+
+I wish I had more time to do more or even all of the Ruby review challenges,
+but I don't. I'm going to have my hands full this week dealing with real life
+issues that have to be resolved before we go onsite, that I'm not going to have
+time to do anything more than the minimum required for week 9. That's unfortunate,
+because I can see that all of the review challenges are interesting and likely
+relevant to what we'll be doing in Phase 1. I'm hoping that enough of us will
+have done different ones that once we get onsite, _someone_ will have done each
+of them, and will be able to bring those experiences to our work onsite.
+
+
+=end
