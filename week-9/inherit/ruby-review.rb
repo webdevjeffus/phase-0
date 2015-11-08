c4aaa090110406492f5b5c262c382e834c2f6281
Jeff George <webdevjeffus@gmail.com>
2015-11-08 06:21:58 -0500

    9.5 Inherit: Add refactored solution.

--- week-9/inherit/ruby-review.rb
+++ week-9/inherit/ruby-review.rb
@@ -3,7 +3,8 @@
 # I worked on this challenge by myself.
 
 
-# Pseudocode
+################################## Pseudocode #################################
+
 =begin
 class GlobalCohort
   attributes:
@@ -27,16 +28,15 @@ class LocalCohort
 
 class Student
   attributes:
-    github
+    first_name
+    last_name
+    local_cohort
     email
-    tuition_paid?
-    integrity?
-    p0_start_date
-    phases_repeated
 =end
 
-# Initial Solution
+############################# Initial Solution ################################
 
+=begin
 require 'date'
 
 class GlobalCohort
@@ -56,9 +56,39 @@ class GlobalCohort
     self.local_cohorts[city_key] = LocalCohort.new(city_string, start_date)
   end
 
-  def enroll_student(first_name, last_name, local_cohort)
+  def add_student(first_name, last_name, local_cohort)
     self.local_cohorts[local_cohort].students.push Student.new(first_name, last_name, local_cohort)
+  end
 
+  def remove_student(first_name, last_name)
+    self.local_cohorts.each_key do |cohort|
+      local_cohorts[cohort].students.each do |student|
+        if student.first_name == first_name && student.last_name == last_name
+          local_cohorts[cohort].students.delete student
+          puts "#{first_name} #{last_name} has been removed from the #{local_cohorts[cohort].name} cohort."
+        end
+      end
+    end
+  end
+
+  def add_student_email(first_name, last_name, email)
+    self.local_cohorts.each_key do |cohort|
+      local_cohorts[cohort].students.each_index do |student|
+        if local_cohorts[cohort].students[student].first_name == first_name &&
+              local_cohorts[cohort].students[student].last_name == last_name
+          local_cohorts[cohort].students[student].email = email
+          puts "Email address added for #{first_name} #{last_name} in the the #{local_cohorts[cohort].name} cohort."
+        end
+      end
+    end
+  end
+
+  def get_email_list
+    email_list = []
+    self.local_cohorts.each_key do |cohort|
+      email_list << local_cohorts[cohort].get_email_list
+    end
+    return email_list
   end
 
   def immersive_start_date
@@ -69,22 +99,51 @@ class GlobalCohort
     @p0_start_date + 130
   end
 
-  def num_of_students
+  def currently_in_phase
+    case Date.today - @p0_start_date
+    when 0..63
+      return "Phase 0"
+    when 63..84
+      return "Phase 1"
+    when 84..105
+      return "Phase 2"
+    when 105..126
+      return "Phase 3"
+    when 126..130
+      return "Career Week"
+    else
+      return "The #{@name} are not an active cohort."
+    end
+  end
 
+  def graduated?
+    (Date.today - @p0_start_date) > 130 ? true : false
+  end
+
+  def num_of_students
+    student_count = 0
+    self.local_cohorts.each_key do |cohort|
+      local_cohorts[cohort].students.each do |student|
+        student_count += 1
+      end
+    end
+    return student_count
   end
 
   def display_cohort_info
+    puts '=========================================='
+    puts @name
     puts "Start Phase 0: #{@p0_start_date}"
     puts "Start Immersive: #{immersive_start_date}"
     puts "Graduate: #{graduation_date}"
-    # puts copperheads.local_cohorts[:boise].num_of_students
-    puts '------------------------------------------'
+    puts "Enrollment: #{self.num_of_students}"
+    puts "Current Phase: #{currently_in_phase}"
   end
 
   def display_local_cohorts_info
-    self.local_cohorts.each do |local_cohort|
-      local_cohort.display_cohort_info
-      local_cohort.display_student_info
+    self.local_cohorts.each_key do |local_cohort|
+      local_cohorts[local_cohort].display_cohort_info
+      local_cohorts[local_cohort].display_student_info
     end
   end
 
@@ -96,17 +155,35 @@ class LocalCohort < GlobalCohort
   attr_accessor :students
 
   def initialize(city_string, start_date)
-    @city = city_string
+    @name = city_string
     @p0_start_date = start_date
     @students = []
   end
 
-  def display_student_info
-    self.students.each do  |student|
-      print first_name + ' '
-      puts last_name
+  def num_of_students
+    student_count = 0
+    self.students.each do |student|
+      student_count += 1
     end
-    puts "=============================="
+    return student_count
+  end
+
+  def display_student_info
+    puts '------------------------------------------'
+    self.students.each do  |student|
+      print student.first_name + ' '
+      puts student.last_name
+    end
+  end
+
+  def get_email_list
+    email_list = []
+    self.students.each_index do |index|
+      if students[index].email
+        email_list << students[index].email
+      end
+    end
+    return email_list
   end
 
 end
@@ -114,70 +191,196 @@ end
 
 class Student
   attr_reader :first_name, :last_name, :local_cohort
+  attr_accessor :email
 
   def initialize(first_name, last_name, local_cohort)
     @first_name = first_name
     @last_name = last_name
     @local_cohort = local_cohort
+    @email = nil
+  end
+end
+=end
+
+############################### Refactored Solution ###########################
+
+require 'date'
+
+class GlobalCohort
+  attr_reader :p0_start_date, :name, :local_cohorts, :num_of_students
+
+  def initialize(start_year,start_month,start_date, name)
+    @p0_start_date = Date.new(start_year,start_month,start_date)
+    @name = name
+    @local_cohorts = {}
+  end
+
+  def create_local_cohort(city_key, city_string, start_date=@p0_start_date)
+    self.local_cohorts[city_key] = LocalCohort.new(city_string, start_date)
+  end
+
+  def add_student(first_name, last_name, local_cohort)
+    self.local_cohorts[local_cohort].students.push Student.new(first_name, last_name, local_cohort)
+  end
+
+  def remove_student(first_name, last_name)
+    self.local_cohorts.each_key do |cohort|
+      local_cohorts[cohort].students.each do |student|
+        if student.first_name == first_name && student.last_name == last_name
+          local_cohorts[cohort].students.delete student
+          puts "#{first_name} #{last_name} has been removed from the #{local_cohorts[cohort].name} cohort."
+        end
+      end
+    end
+  end
+
+  def add_student_email(first_name, last_name, email)
+    self.local_cohorts.each_key do |cohort|
+      local_cohorts[cohort].students.each_index do |student|
+        if local_cohorts[cohort].students[student].first_name == first_name &&
+              local_cohorts[cohort].students[student].last_name == last_name
+          local_cohorts[cohort].students[student].email = email
+          puts "Email address added for #{first_name} #{last_name} in the the #{local_cohorts[cohort].name} cohort."
+        end
+      end
+    end
+  end
+
+  def get_email_list
+    email_list = []
+    self.local_cohorts.each_key do |cohort|
+      email_list << local_cohorts[cohort].get_email_list
+    end
+    return email_list
+  end
+
+  def immersive_start_date
+    @p0_start_date + 63
+  end
+
+  def graduation_date
+    @p0_start_date + 130
+  end
+
+  def currently_in_phase
+    case Date.today - @p0_start_date
+    when 0..63 then return "Phase 0"
+    when 63..84 then return "Phase 1"
+    when 84..105 then return "Phase 2"
+    when 105..126 then return "Phase 3"
+    when 126..130 then return "Career Week"
+    else return "The #{@name} are not an active cohort."
+    end
+  end
+
+  def graduated?
+    (Date.today - @p0_start_date) > 130 ? true : false
+  end
+
+  def num_of_students
+    student_count = 0
+    self.local_cohorts.each_key do |cohort|
+      student_count += local_cohorts[cohort].students.length
+    end
+    return student_count
+  end
+
+  def display_cohort_info
+    puts '=========================================='
+    puts @name
+    puts "Start Phase 0: #{@p0_start_date}"
+    puts "Start Immersive: #{immersive_start_date}"
+    puts "Graduate: #{graduation_date}"
+    puts "Enrollment: #{self.num_of_students}"
+    puts "Current Phase: #{currently_in_phase}"
+  end
+
+  def display_local_cohorts_info
+    self.local_cohorts.each_key do |local_cohort|
+      local_cohorts[local_cohort].display_cohort_info
+      local_cohorts[local_cohort].display_student_info
+    end
+  end
+
+end
+
+
+class LocalCohort < GlobalCohort
+  attr_reader :city, :p0_start_date
+  attr_accessor :students
+
+  def initialize(city_string, start_date)
+    @name = city_string
+    @p0_start_date = start_date
+    @students = []
+  end
+
+  def num_of_students
+    self.students.length
+  end
+
+  def display_student_info
+    puts '------------------------------------------'
+    self.students.each { |student| puts student.first_name + ' ' + student.last_name }
+  end
+
+  def get_email_list
+    email_list = []
+    self.students.each_index do |index|
+      if students[index].email then email_list << students[index].email end
+    end
+    return email_list
+  end
+
+end
+
+
+class Student
+  attr_reader :first_name, :last_name, :local_cohort
+  attr_accessor :email
+
+  def initialize(first_name, last_name, local_cohort)
+    @first_name = first_name
+    @last_name = last_name
+    @local_cohort = local_cohort
+    @email = nil
   end
 end
 
-# Refactored Solution
 
-
-
-
-# Driver Code
+################################### Driver Code ###############################
 
 copperheads = GlobalCohort.new(2015,9,7,"Copperheads")
 
 copperheads.create_local_cohort(:boise, 'Boise')
-copperheads.enroll_student("Lando", "Calrissian", :boise)
-copperheads.enroll_student("Ahsoka", "Tano", :boise)
+copperheads.add_student("Lando", "Calrissian", :boise)
+copperheads.add_student_email("Lando", "Calrissian","lando@cloudcity.gov")
+copperheads.add_student("Ahsoka", "Tano", :boise)
+copperheads.add_student_email("Ahsoka", "Tano", "atano@jeditemple.edu")
+copperheads.add_student("Luke", "Skywalker", :boise)
+copperheads.add_student_email("Luke", "Skywalker", "farmboy@larsfarms.com")
+copperheads.add_student("Jar Jar", "Binks", :boise)
+copperheads.add_student_email("Jar Jar", "Binks", "senatorbinks@naboo.gov")
 
 copperheads.create_local_cohort(:springfield, 'Springfield')
-copperheads.enroll_student("Bart", "Simpson", :springfield)
-copperheads.enroll_student("Maggie", "Simpson", :springfield)
-copperheads.enroll_student("Lisa", "Simpson", :springfield)
-
-puts copperheads.name
-puts copperheads.p0_start_date
-puts copperheads.immersive_start_date
-puts copperheads.graduation_date
-# puts copperheads.num_of_students
+copperheads.add_student("Bart", "Simpson", :springfield)
+copperheads.add_student_email("Bart", "Simpson", "cowabungakid@gmail.com")
+copperheads.add_student("Maggie", "Simpson", :springfield)
+copperheads.add_student("Lisa", "Simpson", :springfield)
+copperheads.add_student_email("Lisa", "Simpson", "lisalisa2015@gmail.com")
+copperheads.add_student("Ned", "Flanders", :springfield)
+copperheads.add_student_email("Ned", "Flanders", "betternedthanred@leftorium.com")
 
 copperheads.display_cohort_info
 copperheads.display_local_cohorts_info
 
-=begin
-# puts copperheads.local_cohorts
-puts "----------------"
-puts "Local Cohort: #{copperheads.local_cohorts[:boise].city}"
-puts "Start Phase 0: #{copperheads.local_cohorts[:boise].p0_start_date}"
-puts "Start Immersive: #{copperheads.local_cohorts[:boise].immersive_start_date}"
-puts "Graduate: #{copperheads.local_cohorts[:boise].graduation_date}"
-# puts copperheads.local_cohorts[:boise].num_of_students
-puts "Students:"
-copperheads.enroll_student("Lando", "Calrissian", :boise)
-copperheads.enroll_student("Ahsoka", "Tano", :boise)
-print copperheads.local_cohorts[:boise].students[0].first_name + ' '
-puts copperheads.local_cohorts[:boise].students[0].last_name
-print copperheads.local_cohorts[:boise].students[1].first_name + ' '
-puts copperheads.local_cohorts[:boise].students[1].last_name
+copperheads.remove_student("Bart", "Simpson")
 
-puts "----------------"
-puts copperheads.local_cohorts[:springfield].city
-puts "Start Phase 0: " + copperheads.local_cohorts[:springfield].p0_start_date
-puts "Start Immersive: " + copperheads.local_cohorts[:springfield].immersive_start_date
-puts "Graduate: " + copperheads.local_cohorts[:springfield].graduation_date
-puts "Students:"
+copperheads.display_cohort_info
+copperheads.display_local_cohorts_info
+puts "=========================================="
+puts "Copperheads Email Addresses:"
+puts copperheads.get_email_list
 
-print copperheads.local_cohorts[:springfield].students[0].first_name + ' '
-puts copperheads.local_cohorts[:springfield].students[0].last_name
-print copperheads.local_cohorts[:springfield].students[1].first_name + ' '
-puts copperheads.local_cohorts[:springfield].students[1].last_name
-print copperheads.local_cohorts[:springfield].students[2].first_name + ' '
-puts copperheads.local_cohorts[:springfield].students[2].last_name
-=end
 
-# Reflection
+################################# Reflection #################################
